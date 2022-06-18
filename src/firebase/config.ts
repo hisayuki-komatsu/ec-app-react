db211a23bf56be144038be386219d9a2af291adc
hisa <hisayuki.k8@gmail.com>
2022-06-18 17:16:27 +0900

    add firebase config

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDZ_xP4XRt6Fn7nJ4f2_XiV3vTp8T47jeE",
+  authDomain: "ec-app-dev-b68cf.firebaseapp.com",
+  projectId: "ec-app-dev-b68cf",
+  storageBucket: "ec-app-dev-b68cf.appspot.com",
+  messagingSenderId: "358466697098",
+  appId: "1:358466697098:web:c01e4325cd1df2d886ba73"
+}
